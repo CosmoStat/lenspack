c2f6cedead8d93419ecf40c26355fae8aea3c355
AJANI Virginia <virginia.ajani@cea.fr>
2021-06-11 17:08:16 +0200

    solve PEP8 errors

--- lenspack/starlet_l1norm.py
+++ lenspack/starlet_l1norm.py
@@ -13,11 +13,9 @@ from lenspack.image.transforms import starlet2d
 
 
 def noise_coeff(image, nscales):
-    
-    """Compute the noise coefficients $\sigma_j$ 
+    """Compute the noise coefficients :math:`\sigma_j`
        to get the estimate of the noise at the scale j
        following Starck and Murtagh (1998).
-       
     Parameters
     ----------
     image : array_like
@@ -25,24 +23,20 @@ def noise_coeff(image, nscales):
     nscales : int
         Number of wavelet scales to compute. Should not exceed log2(N), where
         N is the smaller of the two input dimensions.
-    
     Returns
     -------
     sigma_j : numpy.ndarray
         Values of the standard deviation of the noise at scale j
     """
-         
-    noise_sigma=np.random.randn(image.shape[0],image.shape[0])
-    noise_wavelet=starlet2d(noise_sigma, nscales)
-    sigma_j=np.array([np.std(scale) for scale in noise_wavelet])
+    noise_sigma = np.random.randn(image.shape[0], image.shape[0])
+    noise_wavelet = starlet2d(noise_sigma, nscales)
+    sigma_j = np.array([np.std(scale) for scale in noise_wavelet])
     return sigma_j
 
 
 def get_l1norm_noisy(image, noise, nscales, nbins):
-    
     """Compute the starlet $\ell_1$-norm of a noisy image
-       following Eq. (1) of https://arxiv.org/abs/2101.01542. 
-       
+       following Eq. (1) of https://arxiv.org/abs/2101.01542.
     Parameters
     ----------
     image : array_like
@@ -54,36 +48,32 @@ def get_l1norm_noisy(image, noise, nscales, nbins):
         N is the smaller of the two input dimensions.
     nbins : int
         Number of bins in S/N desired for the summary statistic
-    
     Returns
     -------
         bins_snr, starlet_l1norm : tuple of 1D numpy arrays
         Bin centers in S/N and Starlet $\ell_1$-norm of the noisy image
     """
 
-    #add noise to noiseless image
-    image_noisy=image+noise
-    
-    #perform starlet decomposition
+    # add noise to noiseless image
+    image_noisy = image + noise
+    # perform starlet decomposition
     image_starlet = starlet2d(image_noisy, nscales)
-
     # estimate of the noise
     noise_estimate = mad_std(image_noisy)
-    
-    std_coeff=noise_coeff(image, nscales)
+    std_coeff = noise_coeff(image, nscales)
 
     l1_coll = []
     bins_coll = []
     for image_temp, std_co in zip(image_starlet, std_coeff):
 
-        std_scalej = std_co*noise_estimate
+        std_scalej = std_co * noise_estimate
 
-        snr = image_temp/std_scalej
-        thresholds_snr = np.linspace(np.min(snr), np.max(snr), nbins+1)
-        bins_snr=0.5*(thresholds_snr[:-1] + thresholds_snr[1:])
+        snr = image_temp / std_scalej
+        thresholds_snr = np.linspace(np.min(snr), np.max(snr), nbins + 1)
+        bins_snr = 0.5 * (thresholds_snr[:-1] + thresholds_snr[1:])
         digitized = np.digitize(snr, thresholds_snr)
-        bin_l1_norm = [np.sum(np.abs(snr[digitized == i])) for i in range(1, len(thresholds_snr))]
+        bin_l1_norm = [np.sum(np.abs(snr[digitized == i]))
+                       for i in range(1, len(thresholds_snr))]
         l1_coll.append(bin_l1_norm)
         bins_coll.append(bins_snr)
-        
     return np.array(bins_coll), np.array(l1_coll)
